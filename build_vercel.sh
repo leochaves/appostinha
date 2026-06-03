26a72ff9f33707c5470a5997fd1625f6b8147d05
Leonardo Freitas <leonardo.freitas@tjmg.jus.br>
2026-06-03 19:00:18 -0300

    fix: install flutter in HOME to avoid permission issue

--- build_vercel.sh
+++ build_vercel.sh
@@ -2,11 +2,11 @@
 set -e
 
 FLUTTER_VERSION="3.32.1"
-FLUTTER_DIR="/opt/flutter"
+FLUTTER_DIR="$HOME/flutter"
 
 if [ ! -d "$FLUTTER_DIR" ]; then
   curl -fsSL "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz" \
-    | tar xJf - -C /opt
+    | tar xJf - -C "$HOME"
 fi
 
 export PATH="$PATH:$FLUTTER_DIR/bin"
